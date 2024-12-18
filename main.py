0fb731c640b83b6223b4f07490f2c410455b0b19
VivekCitrusbug <vivek1.citrusbug@gmail.com>
2024-12-18 17:59:27 +0530

    formatted document and added docstring

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.auth.dependencies import create_db_and_tables
-# from src.auth.domain.services import get_password_hash,get_user
 from src.auth.interface import router as auth_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # for loadiing resources that need to be present before the start of the application
+    """
+    for loadiing resources that need to be present before the start of the application
+    """
+
     create_db_and_tables()
     yield
 
@@ -15,6 +18,3 @@ app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(auth_router.router, prefix="/auth", tags=["Users"])
-
-
-
