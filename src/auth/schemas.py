8000e8ed709cde69c0e362bf4cd9c5a5b9a63d14
VivekCitrusbug <vivek1.citrusbug@gmail.com>
2024-12-18 10:56:01 +0530

    database changes

---
+++ src/auth/schemas.py
@@ -0,0 +1,81 @@
+from sqlmodel import SQLModel, Field
+
+
+#############################
+##### PYDANTIC schemas ######
+#############################
+
+
+
+class UserBaseModel(SQLModel):
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    first_name: str | None = Field(default=None)
+    last_name: str | None = Field(default=None)
+    email: str | None = None
+    disabled: bool | None = None
+
+
+class UserPublicModel(UserBaseModel):
+    username:str
+
+class CreateUserModel(UserBaseModel):
+    username:str
+    hashed_password: str
+
+
+class UpdateUserModel(UserBaseModel):
+    name: str | None = None
+    age: int | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    hashed_password: str | None = None
+
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(SQLModel):
+    username: str
+    email: str | None = None
+
+
+
+
+
+# class UserBaseModel(SQLModel):
+#     username: str 
+#     name: str = Field(index=True)
+#     age: int | None = Field(default=None, index=True)
+#     first_name: str | None = Field(default=None)
+#     last_name: str | None = Field(default=None)
+#     email:str | None = None
+#     disabled: bool | None = None
+
+
+# class UserPublicModel(UserBaseModel):
+#     id: int
+
+
+# class CrateUserModel(UserBaseModel):
+#     hashed_password: str
+
+
+# class UpdateUserModel(UserBaseModel):
+#     name: str | None = None
+#     age: int | None = None
+#     first_name: str | None = None
+#     last_name: str | None = None
+#     hashed_password: str | None = None
+
+# class token(SQLModel):
+#     access_token: str
+#     token_type: str
+
+# class token_data(SQLModel):
+#     username:str
+#     email:str | None = None
+
+
